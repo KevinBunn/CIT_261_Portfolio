2bc2f60bfac497804ccbde3a5b8a2f9b1791dea3
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-15 10:55:11 -0600

    button

--- web/assignments.php
+++ web/assignments.php
@@ -60,10 +60,7 @@
     </div>
     <div class="row">
           <div class="col-sm-12">
-            <div id="content">
-                <h1>Assignments</h1>
                 <p><a class="btn btn-secondary btn-sm active" role="button" aria-pressed="true" id="custom-button" href="Project%201/">Project One</a></p>
-            </div>
         </div>
     </div>
 </div>
