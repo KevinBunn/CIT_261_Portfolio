25318f114ec140a3d601147cd4b05b8c070ff926
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-22 11:16:45 -0600

    timebox

--- web/Project 1/main.js
+++ web/Project 1/main.js
@@ -1,14 +1,27 @@
-var listOne = document.getElementById("1");
-var listTwo = document.getElementById("2");
-var listThree = document.getElementById("3");
 
-var colors = ["blue", "green", "orange", "pink", "not yellow"];
-var objects = [listOne, listTwo, listThree];
 
-for (let i = 0; i < objects.length; i++) {
-    console.log(objects[i].innerHTML);
-}
+//var listOne = document.getElementById("1");
+//var listTwo = document.getElementById("2");
+//var listThree = document.getElementById("3");
+//
+//var colors = ["blue", "green", "orange", "pink", "not yellow"];
+//var objects = [listOne, listTwo, listThree];
+//var numbers = [1,5,345,3434,23423];
+//
+//for (let object of objects) {
+//    console.log(object.innerHTML);
+//}
+//
+//for (let i = 0; i < colors.length; i++) {
+//    console.log(colors[i] + "\n");
+//}
 
-for (let i = 0; i < colors.length; i++) {
-    console.log(colors[i] + "\n");
-}
+//const newArray = numbers.map(function(number){
+//  return number * -1;
+//});
+//console.log(newArray);
+//
+// ***OR***
+//
+//const negatives = numbers.map(number => number * -1);
+//console.log(negatives);
