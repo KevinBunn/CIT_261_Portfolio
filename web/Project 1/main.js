79749eec7b385301d20256707645e212a609d0c8
Kevin Bunn <rocketlikeabass@gmail.com>
2018-05-29 21:03:58 -0600

    lots of stuff

--- web/Project 1/main.js
+++ web/Project 1/main.js
@@ -1,4 +1,41 @@
+var taskRow = document.getElementsByClassName("tf-task-content-row");
 
+for (let taskContent of taskRow) {
+	taskContent.addEventListener("touchstart", function() {
+		if (!taskContent.contains(task)) {
+			let newContent = document.createElement('div');
+			newContent.className = "task-content";
+			taskContent.appendChild(newContent);
+		}
+	});
+}
+
+// const hikelist = [
+// 	{
+// 	name: 'Kevin',
+// 	imgSrc: 'gargib',
+// 	imgAlt: 'image that is fake',
+// 	distance: '3 miles',
+// 	difficulty: 'easy',
+// 	decription: 'soo sick hike',
+// 	directions: 'just around the riverbend'
+// 	}
+// ]
+
+// for (let taskContent of taskRow) {
+// 	taskContent.addEventListener("touchstart", function() {
+// 		console.log("click");
+// 		elementToChange = document.getElementById("random-stuff");
+// 		let ul = document.createElement('ul');
+// 		let item = document.createElement('li');
+// 		let image = document.createElement('img');
+// 		image.src = hikelist.imgSrc;
+// 		image.alt = hikelist.imgAlt;
+// 		item.appendChild(image);
+// 		ul.appendChild(item);
+// 		elementToChange.appendChild(ul);
+// 	});
+// }
 
 //var listOne = document.getElementById("1");
 //var listTwo = document.getElementById("2");
@@ -25,3 +62,4 @@
 //
 //const negatives = numbers.map(number => number * -1);
 //console.log(negatives);
+
