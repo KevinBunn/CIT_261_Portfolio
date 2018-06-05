91ae6974290fcd9fb1340e88840f4a7ba344fa8e
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-05 11:13:41 -0600

    arrows added

--- web/Project 1/main.js
+++ web/Project 1/main.js
@@ -1,5 +1,10 @@
 var taskRow = document.getElementsByClassName("tf-task-content-row");
 
+function arrowDrag(event) {
+  event.preventDefault();
+  console.log("dragging");
+}
+
 for (let taskContent of taskRow) {
 	taskContent.addEventListener("touchstart", function(event) {
     let touches = event.touches;
@@ -7,10 +12,28 @@ for (let taskContent of taskRow) {
 			let newContent = document.createElement('div');
       newContent.style.marginLeft = Math.round((touches[0].pageX - 70)/101)*101;
 			newContent.className = "task-content";
+      let leftArrowDiv = document.createElement('div');
+      leftArrowDiv.className = "task-section-arrow-left";
+      let leftArrow = document.createElement('div');
+      leftArrow.className = "task-arrow-left";
+      //leftArrow.style.paddingTop = (littleTing.clientHeight/2);
+      leftArrow.innerHTML = "&#8592";
+      leftArrow.addEventListener("touchmove", arrowDrag, false);
+      leftArrowDiv.appendChild(leftArrow);
+      newContent.appendChild(leftArrowDiv);
+      let rightArrowDiv = document.createElement('div');
+      rightArrowDiv.className = "task-section-arrow-right";
+      let rightArrow = document.createElement('div');
+      rightArrow.addEventListener("touchmove", arrowDrag, false);
+      rightArrow.innerHTML = "&#8594";
+      rightArrow.classList = "task-arrow-right";
+      rightArrowDiv.appendChild(rightArrow);
+      newContent.appendChild(rightArrowDiv);
 			taskContent.appendChild(newContent);
 		}
 	});
 }
+
 
 // const hikelist = [
 // 	{
